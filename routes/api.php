88f484854a613d4b23075ee46cdbd8fe5ed8bb1d
Bourne <xiaopei0206@icloud.com>
2024-04-08 12:38:22 +0800

    fix api;

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\API\ImageController;
 use App\Http\Controllers\API\CarManagerController;
 use App\Http\Controllers\API\PublicController;
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\CarController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
